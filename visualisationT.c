2d3bad18abe6d4a1c2144dceb201eba955eb507f
Erwan Langlais <erwan.langlais@isen-ouest.yncrea.fr>
2026-03-17 11:24:09 +0100

    visu

--- visualisationT.c
+++ visualisationT.c
@@ -1,6 +1,21 @@
 #include "visualisationT.h"
 
-void visualisationT(temp_t myTemp)
-{
-   
+void visualisationT(temp_t myTemp){
+    char *data = "data.txt";
+    char ligne[100];
+    char chauffage[10] = "false";
+
+    FILE *fichier = fopen(data, "r");
+    if(fichier == NULL){
+        return;
+    }
+    
+    if(fgets(ligne, sizeof(ligne), fichier)) {
+        strcpy(chauffage, ligne);
+    }
+    fclose(fichier);
+
+    fichier = fopen(data, "w");
+    fprintf(fichier, "%s\n%.2f\n%.2f\n",chauffage,myTemp.interieure,myTemp.exterieure);
+    fclose(fichier);
 }
