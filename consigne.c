fba438c358ebf92648fed48b32a61f7b4b4e7d1a
Clément Brossaud <clement.brossaud@isen-ouest.yncrea.fr>
2026-03-17 16:20:24 +0100

    changement consigne

--- consigne.c
+++ consigne.c
@@ -1,20 +1,25 @@
 #include "consigne.h"
+#include <stdio.h>
 
-   float consigne(float thermostatPrec_f)
-   {
-      float thermostat_f = thermostatPrec_f;
-      if (fopen(".verrouConsigne", "r") == NULL) {
-         return thermostat_f;
-      }
-      
-      FILE *fichier = fopen("consigne.txt", "r");
-      if (fichier != NULL) {
-         float nouvelleConsigne_f;
-         if (fgets(fichier, "%f", &nouvelleConsigne_f) == 1) {
+float consigne(float thermostatPrec_f) {
+    float thermostat_f = thermostatPrec_f;
+
+    // Si le verrou EST présent, on retourne la consigne précédente sans toucher au fichier
+    FILE *verrou = fopen(".verrouConsigne", "r");
+    if (verrou != NULL) {
+        fclose(verrou);
+        return thermostatPrec_f;
+    }
+
+    // Pas de verrou : on peut lire
+    FILE *fichier = fopen("consigne.txt", "r");
+    if (fichier != NULL) {
+        float nouvelleConsigne_f;
+        if (fscanf(fichier, "%f", &nouvelleConsigne_f) == 1) {
             thermostat_f = nouvelleConsigne_f;
-         }
-         fclose(fichier);
-      }
-      return thermostat_f;
-   
-   }
+        }
+        fclose(fichier);
+    }
+
+    return thermostat_f;
+}
