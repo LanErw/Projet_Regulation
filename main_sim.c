3394ece2b13d202cc9601e2fa6cb85376d43f803
Erwan Langlais <erwan.langlais@isen-ouest.yncrea.fr>
2026-03-18 17:15:08 +0100

    main sim compiled

--- main_sim.c
+++ main_sim.c
@@ -9,16 +9,29 @@
 int main() {
     float consigne_f = 0.0f;
     temp_t myTemp = {0.0f, 0.0f};
-    struct simParam_s* simConstruct(myTemp);
     float puissance = 0.0f;
-    // float tabT[10] = {0.0f};
-    // int nT = 10;
+    float tabT[10];
+    int nT = 0;
     // trace.txt : temps, puis, text, tint
+
     while (1) {
         consigne_f = consigne(consigne_f);
-        myTemp = simCalc(consigne_f, simConstruct);
-        visualisationC(consigne_f);
+        FILE* trace_pf = fopen("trace.txt", "r");
+        if (trace_pf == NULL){
+            printf("Erreur d'ouverture du fichier trace.txt\n");
+            return;
+        }
+        for (int i = 0; i < nT; i++) {
+            tabT[i] = tabT[i+1];
+        }
+        tabT[nT] = myTemp.interieure;
+        puissance = regulationTest(2, consigne_f, tabT, nT);
+        myTemp = simCalc(consigne_f, simConstruct(myTemp));
+        visualisationC(puissance);
         visualisationT(myTemp);
+        if(nT<9){
+            nT++;
+        }
         sleep(1);
     }
 
